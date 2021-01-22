f58776dcf032fae7e56426772bace69147628fbd
Zhalok <zhalokrahman007@gmail.com>
2021-01-22 14:29:41 +0600

    thank me later

--- main/java/com/example/jotno/Database.java
+++ main/java/com/example/jotno/Database.java
@@ -11,15 +11,15 @@ public class Database extends SQLiteOpenHelper {
 
     private Context context;
     private static final String ID="id";
-    private static final String DB_NAME="MYDATABASE";
+    private static final String DB_NAME="MYDATABASE.db";
     private static final String TABLE_NAME="Tutor info";
     private static final String NAME="Name";
     private static final String DEPARTMENT="Department";
-    private static final String GENDER="Gender";
+    private static final String AGE="Age";
     private static final int VERSION=1;
-    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+"("+ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+NAME+" VARCHAR(255),"+ID+" INTEGER);";
+    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+"(ID INTEGER PRIMARY KEY AUTOINCREMENT,"+NAME+" VARCHAR(255),"+AGE+" INTEGER);";
     public Database(Context context) {
-        super(context, DB_NAME, null, 1);
+        super(context, DB_NAME, null, VERSION);
         this.context=context;
 
     }
@@ -27,8 +27,10 @@ public class Database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         try {
+
             Toast.makeText(context,"Oncreatemethod is called",Toast.LENGTH_SHORT).show();
             sqLiteDatabase.execSQL(CREATE_TABLE);
+
         }catch (Exception e)
         {
             Toast.makeText(context,"sorry there is some error",Toast.LENGTH_SHORT).show();
